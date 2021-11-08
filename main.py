b194fb928976ec32f84343a53138b9a939ad18ec
thongtrinh59 <thongtrinh49@gmail.com>
2021-11-07 19:33:25 -0800

    all

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Resource, Api
+from flask_restx import fields
+from flask_restx import inputs
+from flask_restx import reqparse
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import inspect
+from sqlalchemy.orm import sessionmaker
+import copy
+
+
+import json
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://root:root@dbbooking/main'
+
+db = SQLAlchemy(app)
+engine = create_engine('postgresql://root:root@dbbooking/main')
+Session = sessionmaker(bind = engine)
+session = Session()
+
+
+@app.route('/test/<cinemaname>')
+def get_info_cinema(cinemaname):
+
+    with engine.connect() as con:
+        q_str = "SELECT * FROM cinema;"
+        rs = con.execute(q_str)
+        for _ in rs:
+            print(_)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
